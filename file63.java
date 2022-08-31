c2ad0a9d99e22632fbf67c512eec069d7fd771f4
Uppugandla,Prathyusha <pathu0404@gmail.com>
2022-08-31 15:31:44 -0500

    inserting 4th file in repo7

---
+++ file63.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CodesCracker
+{
+   public static void main(String[] args)
+   {
+      Scanner s = new Scanner(System.in);
+      
+      System.out.print("Enter the First Number: ");
+      int a = s.nextInt();
+      System.out.print("Enter the Second Number: ");
+      int b = s.nextInt();
+      
+      System.out.println("\n----Before Swap----");
+      System.out.println("a = " +a);
+      System.out.println("b = " +b);
+      
+      int temp = a;
+      a = b;
+      b = temp;
+      
+      System.out.println("\n----After Swap----");
+      System.out.println("a = " +a);
+      System.out.println("b = " +b);
+   }
+}
